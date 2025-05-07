b22a7c7d859376ff9824c3e74bf0821138bf686c
Dimitrios Psaroudakis <dimitrios.psaroudakis@gmail.com>
2025-05-07 15:00:11 +0200

    created a rust installer

---
+++ installer/target/debug/build/rustix-e02fdb691c5dcee3/build_script_build-e02fdb691c5dcee3.d
@@ -0,0 +1,5 @@
+/home/Lord_Worm/Documents/Thomas More/Courses/Y1S2/Practice Enterprice/DormNet/installer/target/debug/build/rustix-e02fdb691c5dcee3/build_script_build-e02fdb691c5dcee3: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustix-1.0.7/build.rs
+
+/home/Lord_Worm/Documents/Thomas More/Courses/Y1S2/Practice Enterprice/DormNet/installer/target/debug/build/rustix-e02fdb691c5dcee3/build_script_build-e02fdb691c5dcee3.d: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustix-1.0.7/build.rs
+
+/home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustix-1.0.7/build.rs:
