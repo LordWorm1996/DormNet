1fb7d463f486efb512952b5f71dc31eaedf1226f
Dimitrios Psaroudakis <dimitrios.psaroudakis@gmail.com>
2025-05-08 10:19:06 +0200

    cleanup

--- installer/target/debug/deps/cfg_if-cf0a0885cbb0f59b.d
+++
@@ -1,5 +0,0 @@
-/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/deps/libcfg_if-cf0a0885cbb0f59b.rmeta: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/deps/cfg_if-cf0a0885cbb0f59b.d: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs:
--- installer/target/debug/deps/once_cell-917f655b4610b7fa.d
+++
@@ -1,7 +0,0 @@
-/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/deps/libonce_cell-917f655b4610b7fa.rmeta: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs
-
-/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/deps/once_cell-917f655b4610b7fa.d: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs
-
-/home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs:
-/home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/imp_std.rs:
-/home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/race.rs:
