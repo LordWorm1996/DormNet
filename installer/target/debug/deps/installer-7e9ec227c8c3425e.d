e50dd4e94235f9c9ebbc2340f640d3a58a28872c
Dimitrios Psaroudakis <dimitrios.psaroudakis@gmail.com>
2025-05-13 07:21:49 +0200

    updated gitignore

---
+++ installer/target/debug/deps/installer-7e9ec227c8c3425e.d
@@ -0,0 +1,5 @@
+/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/deps/libinstaller-7e9ec227c8c3425e.rmeta: src/main.rs
+
+/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/deps/installer-7e9ec227c8c3425e.d: src/main.rs
+
+src/main.rs:
