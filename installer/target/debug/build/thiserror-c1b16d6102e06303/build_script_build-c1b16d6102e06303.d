9418fa98611a44629c91cf6051191e8835bbbf4a
Dimitrios Psaroudakis <dimitrios.psaroudakis@gmail.com>
2025-05-13 07:21:02 +0200

    updated gitignore

---
+++ installer/target/debug/build/thiserror-c1b16d6102e06303/build_script_build-c1b16d6102e06303.d
@@ -0,0 +1,5 @@
+/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/build/thiserror-c1b16d6102e06303/build_script_build-c1b16d6102e06303: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/build.rs
+
+/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/build/thiserror-c1b16d6102e06303/build_script_build-c1b16d6102e06303.d: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/build.rs
+
+/home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/build.rs:
---
+++ installer/target/debug/deps/thiserror-84e4274dcffe1042.d
@@ -0,0 +1,7 @@
+/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/deps/libthiserror-84e4274dcffe1042.rmeta: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/lib.rs /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/aserror.rs /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/display.rs
+
+/home/Lord_Worm/Documents/Programming/DormNet/installer/target/debug/deps/thiserror-84e4274dcffe1042.d: /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/lib.rs /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/aserror.rs /home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/display.rs
+
+/home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/lib.rs:
+/home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/aserror.rs:
+/home/Lord_Worm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/display.rs:
